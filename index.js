ac3b6d0cd658c772fc72acb36d8207b84b430459
Patrick <patrickyoel13@gmail.com>
2023-09-21 17:56:40 -0300

    actualizacion del contenido

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 
+const esValidoNombres = (nombre, apellido) => {
+    return /^[a-zA-ZáÁéÉíÍóÓúÚüÜñÑ\s]+$/.test(nombre) && /^[a-zA-ZáÁéÉíÍóÓúÚüÜñÑ\s]+$/.test(apellido);
+}
+
+const edadEsvalido = (edad, edadmin, edadmax) => {
+    return !isNaN(edad) && edad >= edadmin && edad <= edadmax;
+}
+
+const dniEsValido = (dni) => {
+    return !isNaN(dni) && /^[0-9a-zA-Z]{8}$/.test(dni);
+}
+
+const materiaEsValido = (materia) => {
+    return /^[a-zA-ZáÁéÉíÍóÓúÚüÜñÑ\s]+$/.test(materia);
+}
+
 class Persona {
     constructor (nombre, apellido, edad) {
         this.nombre = nombre,
@@ -47,6 +63,7 @@ const menus = {
     alumnos: document.getElementById ("menuAlumnos"),
     datosAlumnos: document.getElementById ("datosAlumnos"),
     agregarAlumno: document.getElementById ("agregarAlumno"),
+    actualizarAlumnoAlumno: document.getElementById ("actualizarAlumno"),
     profesores: document.getElementById ("menuProfesores"),
     datosProfesores: document.getElementById ("datosProfesores"),
     agregarProfesor: document.getElementById ("agregarProfesor")
@@ -64,6 +81,7 @@ const buttons = {
     irIngresarProfesor: document.getElementById ("irIngresarProfesor"),
     volverDeAlumnos: document.getElementById ("volverDeAlumnos"),
     volverDeDatosAlumnos: document.getElementById ("volverDeDatosAlumnos"),
+    volverDeEditarAlumno: document.getElementById ("volverDeEditarAlumno"),
     volverDeIngresoAlumno: document.getElementById ("volverDeIngresoAlumno"),
     volverDeProfesores: document.getElementById ("volverDeProfesores"),
     volverDeDatosProfesores: document.getElementById ("volverDeDatosProfesores"),
@@ -75,6 +93,13 @@ const inputsAlumnos = {
     apellido: document.getElementById ("inputLastName"),
     edad: document.getElementById ("inputAge"),
     dni: document.getElementById ("inputdni")
+}
+
+const inputsAlumnosMod = {
+    nombre: document.getElementById ("inputNameMod"),
+    apellido: document.getElementById ("inputLastNameMod"),
+    edad: document.getElementById ("inputAgeMod"),
+    dni: document.getElementById ("inputdniMod")
 }
 
 const inputsProfesores = {
@@ -120,12 +145,12 @@ buttons.irDatosAlumnos.onclick = () => {
             <p class="m-0">${alumno.edad}</p>
             <p class="m-0">${alumno.dni}</p>
             <div>
-                <button class="me-3">
+                <button class="irEditarAlumno me-3">
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
                     <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
                     </svg>
                 </button>
-                <button>
+                <button class="irBorrarAlumno">
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                         <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                     </svg>
@@ -133,6 +158,11 @@ buttons.irDatosAlumnos.onclick = () => {
             </div>
         </div>`;
     }
+}
+
+buttons.volverDeEditarAlumno.onclick = () => {
+    menus.modificarAlumno.classList.replace ("d-block", "d-none");
+    menus.datosAlumnos.classList.replace ("d-none", "d-block");
 }
 
 buttons.volverDeDatosAlumnos.onclick = () => {
@@ -210,22 +240,6 @@ buttons.volverDeDatosProfesores.onclick = () => {
 buttons.volverDeProfesores.onclick = () => {
     menus.profesores.classList.replace ("d-block", "d-none");
     menus.principal.classList.replace ("d-none", "d-block");
-}
-
-const esValidoNombres = (nombre, apellido) => {
-    return /^[a-zA-ZñÑ]+$/.test(nombre) && /^[a-zA-ZñÑ]+$/.test(apellido);
-}
-
-const edadEsvalido = (edad, edadmin, edadmax) => {
-    return !isNaN(edad) && edad >= edadmin && edad <= edadmax;
-}
-
-const dniEsValido = (dni) => {
-    return !isNaN(dni) && /^[0-9a-zA-Z]{8}$/.test(dni);
-}
-
-const materiaEsValido = (materia) => {
-    return /^[a-zA-ZñÑ]+$/.test(materia);
 }
 
 const validarAlumno = document.getElementById ("validarAlumno");
