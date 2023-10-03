6d1fe30a3d7bedb24026cf509316323a7073efe3
Patrick <patrickyoel13@gmail.com>
2023-10-03 16:49:32 -0300

    validaciones

--- index.js
+++ index.js
@@ -102,23 +102,7 @@ const inputsProfesores = {
 
 // Funcion de navegacion 
 
-const navegar = (menuActual, menuObjetivo) => {
-    menus[menuActual].classList.replace ("d-block", "d-none");
-    menus[menuObjetivo].classList.replace ("d-none", "d-block");
-}
-
-buttons.salir.onclick = () => navegar ("principal", "salir");
-
-buttons.confirmarSalir.onclick = () => {
-    window.close();
-}
-
-buttons.irPrincipal.onclick = () => navegar ("salir", "principal");
-
-buttons.irAlumnos.onclick = () => navegar ("principal", "alumnos");
-
-buttons.irDatosAlumnos.onclick = () => {
-    navegar("alumnos", "datosAlumnos");
+const construirListaAlumnos = () => {
 
     let listaAlumnos = document.getElementById ("listaAlumnos")
 
@@ -144,7 +128,7 @@ buttons.irDatosAlumnos.onclick = () => {
                         <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
                         </svg>
                     </button>
-                    <button class="irBorrarAlumno">
+                    <button class="irBorrarAlumno" data-index="${i}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                         </svg>
@@ -182,6 +166,57 @@ buttons.irDatosAlumnos.onclick = () => {
             inputsAlumnos.dni.value = alumnoActual.dni;
         }
     });
+
+    borrarAlumno.forEach ((button) => {
+        button.onclick = function(){
+
+            const botonActual = this;
+
+            const indexBotonBorrar = parseInt(botonActual.dataset.index, 10);
+            
+            alumnoIndex = indexBotonBorrar;
+
+            const alumnoActual = alumnos[indexBotonBorrar];
+
+            if (indexBotonBorrar >=0 && indexBotonBorrar < alumnos.length) {
+
+                alumnos.splice (indexBotonBorrar, 1);
+
+                localStorage.setItem("AlumnoStorage", JSON.stringify(alumnos));
+
+                construirListaAlumnos();
+
+                Swal.fire(
+                    'Eliminación exitosa',
+                    'El alumno ha sido eliminado de la lista',
+                    'success'
+                );
+            }
+        }
+    })
+}
+
+const navegar = (menuActual, menuObjetivo) => {
+    menus[menuActual].classList.replace ("d-block", "d-none");
+    menus[menuObjetivo].classList.replace ("d-none", "d-block");
+}
+
+buttons.salir.onclick = () => navegar ("principal", "salir");
+
+buttons.confirmarSalir.onclick = () => {
+    window.close();
+}
+
+buttons.irPrincipal.onclick = () => navegar ("salir", "principal");
+
+buttons.irAlumnos.onclick = () => navegar ("principal", "alumnos");
+
+buttons.irDatosAlumnos.onclick = () => {
+
+    navegar("alumnos", "datosAlumnos");
+
+    construirListaAlumnos();
+
 }
 
 
@@ -204,7 +239,13 @@ buttons.irIngresarAlumno.onclick = () => {
     buttons.volverDeIngresoAlumno.parentNode.classList.replace ("d-none", "d-block");
 }
 
-buttons.volverDeActualizarAlumno.onclick = () => navegar ("agregarAlumno", "datosAlumnos");
+buttons.volverDeActualizarAlumno.onclick = () => {
+
+    navegar ("agregarAlumno", "datosAlumnos");
+
+    construirListaAlumnos();
+
+}
 
 buttons.volverDeIngresoAlumno.onclick = () => navegar ("agregarAlumno", "alumnos");
 
