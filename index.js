930bd123675f8c4bce919a3644009a59ac59c2d1
Patrick <patrickyoel13@gmail.com>
2023-10-04 19:41:56 -0300

    profesores update

--- index.js
+++ index.js
@@ -44,6 +44,8 @@ let alumnoIndex;
 
 const profesores = JSON.parse(localStorage.getItem("ProfesorStorage")) || [];
 
+let profesorIndex;
+
 const menus = {
     principal: document.getElementById ("menuPrincipal"),
     salir: document.getElementById ("menuSalir"),
@@ -68,6 +70,8 @@ const buttons = {
     irProfesores: document.getElementById ("irProfesores"),
     irDatosProfesores: document.getElementById ("irDatosProfesores"),
     irIngresarProfesor: document.getElementById ("irIngresarProfesor"),
+    validarProfesor: document.getElementById ("validarProfesor"),
+    actualizarProfesor: document.getElementById ("actualizarProfesor"),
     volverDeAlumnos: document.getElementById ("volverDeAlumnos"),
     volverDeDatosAlumnos: document.getElementById ("volverDeDatosAlumnos"),
     volverDeEditarAlumno: document.getElementById ("volverDeEditarAlumno"),
@@ -75,7 +79,8 @@ const buttons = {
     volverDeActualizarAlumno: document.getElementById ("volverDeActualizarAlumno"),
     volverDeProfesores: document.getElementById ("volverDeProfesores"),
     volverDeDatosProfesores: document.getElementById ("volverDeDatosProfesores"),
-    volverDeIngresoProfesor: document.getElementById ("volverDeIngresoProfesor")
+    volverDeIngresoProfesor: document.getElementById ("volverDeIngresoProfesor"),
+    volverDeActualizarProfesor: document.getElementById ("volverDeActualizarProfesor")
 }
 
 const inputsAlumnos = {
@@ -116,10 +121,11 @@ const construirListaAlumnos = () => {
             'no se han encontrado datos',
             'question'
             );
-            
+
     } else {
 
         for (let i = 0; i < alumnos.length; i++){
+
             listaAlumnos.innerHTML += `
             <div class="d-flex justify-content-between align-items-center m-5">
                 <img src="./images/lapiz-de-usuario-32.png" alt="">
@@ -143,7 +149,7 @@ const construirListaAlumnos = () => {
     }
 
     const editarAlumnoBtns = document.querySelectorAll(".irEditarAlumno");
-    const borrarAlumno = document.querySelectorAll(".irBorrarAlumno");
+    const borrarAlumnoBtns = document.querySelectorAll(".irBorrarAlumno");
 
     editarAlumnoBtns.forEach ((button) => {
         button.onclick = function(){
@@ -171,7 +177,7 @@ const construirListaAlumnos = () => {
         }
     });
 
-    borrarAlumno.forEach ((button) => {
+    borrarAlumnoBtns.forEach ((button) => {
         button.onclick = function(){
 
             const botonActual = this;
@@ -206,6 +212,81 @@ const construirListaAlumnos = () => {
                     }
                 }
             })
+        }
+    })
+}
+
+// funcion constructora de profesores
+
+const construirListaProfesores = () => {
+
+    let listaProfesores = document.getElementById ("listaProfesores")
+
+    listaProfesores.innerHTML = "";
+
+    if (profesores.length === 0) {
+
+        Swal.fire(
+            '¡Ups!',
+            'no se han encontrado datos',
+            'question'
+            );
+
+    } else {
+
+        for (let i = 0; i < profesores.length; i++){
+
+            listaProfesores.innerHTML += `
+            <div class="d-flex justify-content-between align-items-center m-5">
+                <img src="./images/lapiz-de-usuario-32.png" alt="">
+                <p class="m-0" style="width: 250px;">${profesores[i].nombre} ${profesores[i].apellido}</p>
+                <p class="m-0">${profesores[i].edad}</p>
+                <p class="m-0">${profesores[i].dni}</p>
+                <p class="m-0">${profesores[i].materia}</p>
+                <div>
+                    <button class="me-3 irEditarProfesor" data-index="${i}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+                        <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+                        </svg>
+                    </button>
+                    <button class="irBorrarProfesor" data-index="${i}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                        </svg>
+                    </button>
+                </div>
+            </div>`
+        }
+    }
+
+    const editarProfesorBtns = document.querySelectorAll (".irEditarProfesor");
+    const irBorrarProfesorBtns = document.querySelectorAll (".irBorrarProfesor");
+
+    editarProfesorBtns.forEach ((button) => {
+        button.onclick = function(){
+
+            const botonActual = this;
+
+            const index = botonActual.dataset.index;
+
+            profesorIndex = index;
+
+            const profesorActual = profesores[index];
+
+            navegar ("datosProfesores", "agregarProfesor");
+
+            buttons.validarProfesor.classList.replace ("d-block", "d-none");
+            buttons.actualizarProfesor.classList.replace ("d-none", "d-block");
+
+            buttons.volverDeIngresoProfesor.parentNode.classList.replace ("d-block", "d-none");
+            buttons.volverDeActualizarProfesor.parentNode.classList.replace ("d-none", "d-block");
+
+            inputsProfesores.nombre.value = profesorActual.nombre;
+            inputsProfesores.apellido.value = profesorActual.apellido;
+            inputsProfesores.edad.value = profesorActual.edad;
+            inputsProfesores.dni.value = profesorActual.dni;
+            inputsProfesores.materia.value = profesorActual.materia;
+
         }
     })
 }
@@ -272,43 +353,34 @@ buttons.irDatosProfesores.onclick = () => {
 
     navegar ("profesores", "datosProfesores");
 
-    let listaProfesores = document.getElementById ("listaProfesores")
+    construirListaProfesores();
 
-    if (profesores.length === 0) {
-        Swal.fire(
-            '¡Ups!',
-            'no se han encontrado datos',
-            'question'
-            );
-    } else {
-        listaProfesores.innerHTML = "";
-
-        for ( const profesor of profesores) {
-            listaProfesores.innerHTML += `
-            <div class="d-flex justify-content-between align-items-center m-5">
-                <img src="./images/lapiz-de-usuario-32.png" alt="">
-                <p class="m-0" style="width: 250px;">${profesor.nombre} ${profesor.apellido}</p>
-                <p class="m-0">${profesor.edad}</p>
-                <p class="m-0">${profesor.dni}</p>
-                <p class="m-0">${profesor.materia}</p>
-                <div>
-                    <button class="me-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
-                        <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
-                        </svg>
-                    </button>
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                        </svg>
-                    </button>
-                </div>
-            </div>`
-        }
-    }
 }
 
-buttons.irIngresarProfesor.onclick = () => navegar ("profesores", "agregarProfesor");
+buttons.irIngresarProfesor.onclick = () => {
+
+    inputsProfesores.nombre.value = "";
+    inputsProfesores.apellido.value = "";
+    inputsProfesores.edad.value = "";
+    inputsProfesores.dni.value = "";
+    inputsProfesores.materia.value = "";
+    
+    navegar ("profesores", "agregarProfesor");
+
+    buttons.actualizarProfesor.classList.replace ("d-block", "d-none");
+    buttons.validarAlumno.classList.replace ("d-none", "d-block");
+
+    buttons.volverDeActualizarProfesor.parentNode.classList.replace ("d-block", "d-none");
+    buttons.volverDeIngresoProfesor.parentNode.classList.replace ("d-none", "d-block");
+}
+
+buttons.volverDeActualizarProfesor.onclick = () => {
+
+    navegar ("agregarProfesor", "datosProfesores");
+
+    construirListaProfesores();
+
+}
 
 
 buttons.volverDeIngresoProfesor.onclick = () => navegar ("agregarProfesor", "profesores");
@@ -415,9 +487,8 @@ buttons.actualizarAlumno.onclick = () => {
     }
 }
 
-const validarProfesor = document.getElementById ("validarProfesor");
 
-validarProfesor.onclick = () => {
+buttons.validarProfesor.onclick = () => {
 
     let valorInputNombreTeacher = inputsProfesores.nombre.value.trim();
     let valorInputApellidoTeacher = inputsProfesores.apellido.value.trim();
@@ -435,12 +506,11 @@ validarProfesor.onclick = () => {
             'warning'
             );
 
-    } else if (esValidoNombres(valorInputNombreTeacher, valorInputApellidoTeacher) && edadEsvalido(valorInputEdadTeacher, 22, 64) && dniEsValido(valorInputDniTeacher) && materiaEsValido(valorInputMateria)) {
+    } else if (esValidoNombres(valorInputNombreTeacher, valorInputApellidoTeacher) && edadEsvalido(valorInputEdadTeacher, 22, 64) && dniEsValido(valorInputDniTeacher) && materiaEsValido(valorInputMateria)){
         
         profesores.push (new Profesor (valorInputNombreTeacher, valorInputApellidoTeacher, valorInputEdadTeacher, valorInputDniTeacher, valorInputMateria));
 
         localStorage.setItem ("ProfesorStorage", JSON.stringify(profesores));
-
 
         inputsProfesores.nombre.value = "";
         inputsProfesores.apellido.value = "";
@@ -462,5 +532,55 @@ validarProfesor.onclick = () => {
             'error'
             );
 
+    }
+}
+
+buttons.actualizarProfesor.onclick = () => {
+
+    let valorInputNombreTeacher = inputsProfesores.nombre.value.trim();
+    let valorInputApellidoTeacher = inputsProfesores.apellido.value.trim();
+    let valorInputEdadTeacher = parseInt(inputsProfesores.edad.value);
+    let valorInputDniTeacher = parseInt(inputsProfesores.dni.value);
+    let valorInputMateria = inputsProfesores.materia.value.trim();
+
+    const profesorActual = profesores[profesorIndex];
+
+    const profesorExiste = (profesorActual.dni != valorInputDniTeacher) && profesores.some(profesor => profesor.dni === valorInputDniTeacher);
+
+    if (profesorExiste) {
+        Swal.fire(
+            'Ingreso invalido',
+            'el profesor que usted desea agregar, ya existe',
+            'warning'
+        );
+    } else if (esValidoNombres(valorInputNombreTeacher, valorInputApellidoTeacher) && edadEsvalido(valorInputEdadTeacher, 22, 64) && dniEsValido(valorInputDniTeacher) && materiaEsValido(valorInputMateria)) {
+
+        profesorActual.nombre = valorInputNombreTeacher;
+        profesorActual.apellido = valorInputApellidoTeacher;
+        profesorActual.edad = valorInputEdadTeacher;
+        profesorActual.dni = valorInputDniTeacher;
+        profesorActual.materia = valorInputMateria;
+
+        localStorage.setItem("ProfesorStorage", JSON.stringify(profesores));
+
+        inputsProfesores.nombre.value = "";
+        inputsProfesores.apellido.value = "";
+        inputsProfesores.edad.value = "";
+        inputsProfesores.dni.value = "";
+        inputsProfesores.materia.value = "";
+
+        Swal.fire(
+            'Ingreso exitoso',
+            'el profesor ha sido añadido a la lista',
+            'success'
+        );
+
+    } else {
+
+        Swal.fire(
+            'Ingreso invalido',
+            'los datos proporcionados son incorrectos',
+            'error'
+        );
     }
 }
